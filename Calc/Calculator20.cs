134d3def6ecf0b6d4232ad590c9c7f4d60b74bb8
Класс <class@vyatsu.ru>
2020-02-08 13:51:39 +0300

    calc

---
+++ Calc/Calculator20.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Calculator20
+    {
+   public double First;
+    public string Method;
+
+    public double Action(double second)
+    {
+      switch (Method)
+    {
+       case "+":return First + second;
+       case "-":return First - second;
+       case "*":return First * second;
+       default: return First / second;
+     }
+   }
+  } 
+}
---
+++ Calc/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+ {
+    public partial class Form1 : Form
+     {
+        private Calculator20 calculator=new Calculator20();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+           tbNumber.Text = tbNumber. Text + (sender as Button).Text;
+        }
+
+        private void c_Clisk(object sender, EventArgs e)
+        {
+            calculator = new Calculator20();
+            tbNumber.Text = "";
+        }
+        private void Action_Click(object sender, EventArgs e)
+        {
+            calculator.Method = (sender as Button).Text ;
+            calculator.First = Convert.ToDouble(tbNumber.Text);
+            tbNumber.Text = "";
+        }
+        private void Result_Clisk(object sender, EventArgs e)
+        {
+            double second = Convert.ToDouble(tbNumber.Text);
+            double result = calculator.Action(second);
+            tbNumber.Text = result.ToString();
+        }
+    }
+}
