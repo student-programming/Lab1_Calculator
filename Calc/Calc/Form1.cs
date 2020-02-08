c44ed010139e2a1619def797ca7bddfeb053c793
Класс <class@vyatsu.ru>
2020-02-08 13:23:58 +0300

    adddf

---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class frmCalc : Form
+    {
+        private Calculator calculator = new Calculator();
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+            tbNumber.Text = tbNumber.Text + (sender as Button).Text;
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            calculator = new Calculator();
+            tbNumber.Text = "";
+        }
+
+        private void Action_Click(object sender, EventArgs e)
+        {
+            calculator.Method = (sender as Button).Text;
+            calculator.First = Convert.ToDouble(tbNumber.Text);
+            tbNumber.Text = "";
+
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+            double second = Convert.ToDouble(tbNumber.Text);
+            double result = calculator.Action(second);
+            tbNumber.Text = result.ToString();
+        }
+    }
+}
