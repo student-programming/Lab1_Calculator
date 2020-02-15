c74a55178daadb25fffe7a174a6c1416b075551e
Класс <class@vyatsu.ru>
2020-02-15 09:47:30 +0300

    Text comment

---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class frmCalc : Form
+    {
+        private Calculator calculator = new Calculator();
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+            tbNumber.Text = tbNumber.Text + (sender as Button).Text;
+
+        }
+
+        private void frmCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Action_Click(object sender, EventArgs e)
+        {
+            calculator.Method = (sender as Button).Text;
+            calculator.First = Convert.ToDouble(String.IsNullOrEmpty(tbNumber.Text) ? "0" :
+                tbNumber.Text);
+            tbNumber.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+            double second = Convert.ToDouble(String.IsNullOrEmpty(tbNumber.Text) ? "0" :
+                tbNumber.Text);
+            double result = calculator.Action(second);
+            tbNumber.Text = result.ToString();
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            calculator = new Calculator();
+            tbNumber.Text = "";
+        }
+
+        
+    }
+}
