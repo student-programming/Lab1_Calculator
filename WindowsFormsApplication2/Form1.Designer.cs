f740cf83541041a442aedb494caa521e14aa2f0b
Класс <class@vyatsu.ru>
2020-02-15 09:47:56 +0300

    Kalck

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 362);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class frmCalc : Form
+    {
+        private Calculator calculator = new Calculator();
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number_Click(object sender, EventArgs e)
+        {
+            tbNumber.Text = tbNumber.Text + (sender as Button).Text;
+
+
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            calculator = new Calculator();
+            tbNumber.Text = "";
+
+        }
+
+        private void Action_Click(object sender, EventArgs e)
+        {
+            calculator.Method = (sender as Button).Text;
+            calculator.First = Convert.ToDouble(String.IsNullOrEmpty (tbNumber.Text) ? "0" : tbNumber.Text);
+            tbNumber.Text = "";
+
+
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+            double second = Convert.ToDouble(String.IsNullOrEmpty(tbNumber.Text) ? "0" : tbNumber.Text);
+            double result = calculator.Action(second);
+            tbNumber.Text = result.ToString();
+
+        }
+
+    }
+}
